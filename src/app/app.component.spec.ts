14f1734e48f156ad536e4a14ec9aa43034e58e16
Mark Goho <markgoho@gmail.com>
2018-05-04 21:29:54 -0400

    Remove unnecessary test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -15,9 +15,4 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  xit(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('Gideon Analytical Labs');
-  }));
 });
