c61c3e2cfd347ec59ae7ea3eeff3fa1c168a69ad
Mark Goho <markgoho@gmail.com>
2018-05-06 16:02:02 -0400

    Add lazy loading to images

--- src/app/shared/containers/recent-posts/recent-posts.component.ts
+++ src/app/shared/containers/recent-posts/recent-posts.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, ElementRef, OnInit } from '@angular/core';
+import { Observable, BehaviorSubject } from 'rxjs';
 import { Post } from '../../../post/models/post.interface';
 import { Store, select } from '@ngrx/store';
 import { PostsState } from '../../../store/reducers/posts.reducer';
@@ -10,10 +10,26 @@ import { getRecentPosts } from '../../../store/selectors/posts.selectors';
   templateUrl: './recent-posts.component.html',
   styleUrls: ['./recent-posts.component.scss'],
 })
-export class RecentPostsComponent {
+export class RecentPostsComponent implements OnInit {
+  options = {
+    rootMargin: '0px',
+    threshold: 1,
+  };
+  inView$: BehaviorSubject<boolean> = new BehaviorSubject(false);
+  observer: IntersectionObserver;
+
   recentPosts$: Observable<Post[]>;
 
-  constructor(private store: Store<PostsState>) {
+  constructor(private store: Store<PostsState>, private element: ElementRef) {
     this.recentPosts$ = this.store.pipe(select(getRecentPosts));
+  }
+
+  ngOnInit() {
+    this.observer = new IntersectionObserver(entries => {
+      entries.forEach((entry: IntersectionObserverEntry) => {
+        this.inView$.next(entry.isIntersecting);
+      });
+    });
+    this.observer.observe(this.element.nativeElement);
   }
 }
