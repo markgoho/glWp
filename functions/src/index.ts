12bf175713f6aa47cdfe35c023161af4f4755b70
Mark Goho <markgoho@gmail.com>
2018-05-19 12:52:05 -0400

    Updated api route

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,5 @@
 import * as functions from 'firebase-functions';
+import * as express from 'express';
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -6,3 +7,18 @@ import * as functions from 'firebase-functions';
 // export const helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+const app = express();
+
+// // Automatically allow cross-origin requests
+// app.use(cors({ origin: true }));
+
+// // Add middleware if you want.
+// app.use(myMiddleware);
+
+// build multiple CRUD interfaces:
+app.get('*', (req, res) => {
+  res.send('This is an api route!');
+});
+
+// Expose Express API as a single Cloud Function:
+exports.myAwesomeFunction = functions.https.onRequest(app);
