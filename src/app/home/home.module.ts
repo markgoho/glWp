9ad7c49d17d62c0fe651f5bcd1d67a6bfcdebd77
Mark Goho <markgoho@gmail.com>
2018-11-24 14:19:55 -0500

    navbar and hero container responsive

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -6,9 +6,10 @@ import { HomeComponent } from './containers/home/home.component';
 import { StepsComponent } from './components/steps/steps.component';
 import { RecentPostsComponent } from './containers/recent-posts/recent-posts.component';
 import { RecentPostComponent } from './components/recent-post/recent-post.component';
+import { HeroComponent } from './components/hero/hero.component';
 
 @NgModule({
   imports: [CommonModule, HomeRoutingModule],
-  declarations: [HomeComponent, StepsComponent, RecentPostsComponent, RecentPostComponent],
+  declarations: [HomeComponent, StepsComponent, RecentPostsComponent, RecentPostComponent, HeroComponent],
 })
 export class HomeModule {}
